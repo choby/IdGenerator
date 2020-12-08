c6678379dbe726cfa284f41076698c65deabc35e
dabiao <choby@outlook.com>
2020-12-08 11:13:21 +0700

    增加示例项目

---
+++ IdGenerator.Sample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using IdGenerator.Obfuscat;
+using IdGenerator.Snowflake;
+
+namespace IdGenerator.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // var worker = new IdWorker(2, 1);
+            var idObfuscator = new IdObfuscator();
+            uint id = uint.MaxValue;
+            while (true)
+            {
+                //ulong id = (ulong)worker.NextId();
+                var feistelID = idObfuscator.Permute(id);
+                var reFeistelID = idObfuscator.Permute(feistelID);
+                var base62 = idObfuscator.PermuteToBase62(id);
+                Console.WriteLine($"ID:{id} , Length:{id.ToString().Length}; feistelID:{feistelID},refeistelID:{reFeistelID},base62:{base62}");
+                if (id != reFeistelID)
+                {
+                    Console.WriteLine($"加解密异常:原始id:{id}, feistelID:{feistelID},refeistelID:{reFeistelID},base62:{base62}");
+                    break;
+                }
+                id--;
+                if (id <= 0) break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
